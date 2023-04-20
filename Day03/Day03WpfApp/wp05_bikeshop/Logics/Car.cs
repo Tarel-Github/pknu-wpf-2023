17d13dec707df0e14c476b77e00e65d56ff5eae8
Tarel-Github <sms_stmax@naver.com>
2023-04-20 17:25:29 +0900

    4월 20일 WPF 3일차 완료

---
+++ Day03/Day03WpfApp/wp05_bikeshop/Logics/Car.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows.Media;
+
+namespace wp05_bikeshop.Logics
+{
+    internal class Car : Notifier // 값이 바뀌는 걸 인지하여서 처리하겠다.
+    {
+        private string names;
+        public string Names {
+            get => names;
+
+            // 프로퍼티를 변경하는 것 
+            set
+            {
+                names = value;
+                OnPropertyChanged("Names");
+            }
+        }    // Auto Property
+        private double speed;
+        public double Speed
+        {
+            get => speed;
+            set
+            {
+                speed = value;
+                OnPropertyChanged(nameof(Speed));   // == "Speed"
+            }
+        }
+        public Color Colorz { get; set; }
+        public Human Driver { get; set; }
+
+    }
+}
---
+++ Day03/Day03WpfApp/wp05_bikeshop/Logics/Human.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Media;
+
+namespace wp05_bikeshop.Logics
+{
+    internal class Human
+    {
+        public string FullName { get; set; }
+        public bool HasLicense { get; set; }
+    }
+}
